a7b9e1c61766b604c7ed67e5f8b32511391e0d0a
8-u8 <itotaka523@gmail.com>
2021-04-18 18:45:57 +0900

    make linear search

---
+++ sources/data_algo/Linear_Search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // get input
+    int N, v;
+    cin >> N >> v;
+    vector<int> a(N);
+
+    for (int i = 0; i < N; ++i)
+        cin >> a[i];
+
+    // linear search
+    bool exist = false; // init value is false
+    for (int i = 0; i < N; ++i)
+    {
+        if (a[i] == v)
+        {
+            exist == true; // if v found flag.
+        }
+    }
+    //output
+    if (exist)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
